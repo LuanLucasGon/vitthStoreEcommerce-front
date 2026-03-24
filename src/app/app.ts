594e634a3073ba5b95cf998f92392d45e3e08b4f
Lucas Luan Gonçalves <81707963+LuanLucasGon@users.noreply.github.com>
2026-03-24 00:20:40 -0300

    feat: Creating footer commponent

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { TopbarComponent } from './components/topbar/topbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopbarComponent],
+  imports: [RouterOutlet, TopbarComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
