e84e1b39737467c125d1906c3c200620751444e1
Lucas Luan Gonçalves <81707963+LuanLucasGon@users.noreply.github.com>
2026-03-23 23:43:59 -0300

    feat: Creating variavles.scss

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,7 +16,7 @@ export const appConfig: ApplicationConfig = {
       theme: {
         preset: Aura,
         options: {
-          darkModeSelector: '.my-app-dark'
+          darkModeSelector: '[data-theme="dark"]'
         }
       }
     }),
