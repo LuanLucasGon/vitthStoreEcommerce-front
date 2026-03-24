1b7c5715742128c42e4dcfe4a1375c32a8238e8d
Lucas Luan Gonçalves <81707963+LuanLucasGon@users.noreply.github.com>
2026-03-24 00:41:13 -0300

    feat: Adding the login button on topbar

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,19 +1,21 @@
-import { Component, inject } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
-import { LucideAngularModule, ShoppingCart, Shirt } from 'lucide-angular';
+import { Component, inject, signal } from '@angular/core';
+import { LucideAngularModule, ShoppingCart, Shirt, Search, User } from 'lucide-angular';
 
 import { CartStore } from '../../state/cart.store';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [ButtonModule, LucideAngularModule],
+  imports: [LucideAngularModule],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss',
 })
 export class TopbarComponent {
   readonly ShoppingCart = ShoppingCart;
   readonly Shirt = Shirt;
+  readonly Search = Search;
+  readonly User = User;
 
   protected readonly cartStore = inject(CartStore);
+  protected readonly searchQuery = signal('');
 }
